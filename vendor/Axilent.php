7fa48e1f90431e6d6c62f17a3d9eeaa361b7a2b2
Loren Davie <loren@axilent.com>
2012-04-11 12:00:46 -0400

    Changed references to main cluster
    Previously wpdev.axilent.net was the endpoint, changing to main cluster
    www.axilent.net.

--- vendor/Axilent.php
+++ vendor/Axilent.php
@@ -27,13 +27,13 @@ class Axilent
      * A template for API Functions
      * @var string 
      */
-    protected $_functionPrototype = "http://wpdev.axilent.net/api/%s/%s/";
+    protected $_functionPrototype = "http://www.axilent.net/api/%s/%s/";
     
     /**
      * A template for API Resources
      * @var string 
      */
-    protected $_resourcePrototype = "http://wpdev.axilent.net/api/resource/%s/%s/";
+    protected $_resourcePrototype = "http://www.axilent.net/api/resource/%s/%s/";
     
     /**
      * The version of the API we're using
@@ -122,7 +122,7 @@ class Axilent
         if($this->_portletKey === null)
             throw new Exception("Portlet key was not provided at initialization of " . __CLASS__);
         
-        return "http://{$this->_portletKey}@wpdev.axilent.net/airtower/portlets/content/?key={$content_key}&content_type=post";
+        return "http://{$this->_portletKey}@www.axilent.net/airtower/portlets/content/?key={$content_key}&content_type=post";
     }
     
     /**
